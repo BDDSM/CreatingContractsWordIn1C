1021315d5eb7aa2d75009deb4d1e897ed7a321dc
Diversus <support@softonit.ru>
2020-06-25 10:35:53 +0300

    Создана папка релизы, файл с инструкцией переименован

--- build_release.bat
+++ build_release.bat
@@ -5,4 +5,4 @@ echo ---------------------------
 
 set version=1.8
 
-"C:\Program Files\7-Zip\7z.exe" a -tzip -ssw -mx9 -r0 release_%version%.zip "*.epf" "*.doc"
+"C:\Program Files\7-Zip\7z.exe" a -tzip -ssw -mx9 -r0 .\releases\release_%version%.zip "*.epf" "*.doc"
